11dbdab81846306c14e9911cd11c64697e8f9f2b
Jake Maranzatto <maranzatto1@yahoo.com>
2018-07-20 16:20:06 -0400

    smol changes
    to silencewav

--- SilenceWav.py
+++ SilenceWav.py
@@ -4,45 +4,75 @@ from scipy.io import wavfile
 import os
 import more_itertools as mit
 
-soundTime = []
-participantSoundTime = {}
 
-counter = 0
-threshold = 1000
+
+def find_ranges(iterable):
+    """Yield range of consecutive numbers."""
+    for group in mit.consecutive_groups(iterable):
+        group = list(group)
+        if len(group) == 1:
+            yield 1000 * group[0], 1000 + 1000 * group[0]
+        else:
+            yield 1000* group[0], 1000 + 1000*group[-1]
+
+            
 
 #obtaining file
-for num in range(1, 33):
-    if num == 11 or num == 13 or num == 14 or num == 16 or num == 21 or num == 22 or num == 26 or num == 27:
-        continue
-    else:
-        filePathwayRead = os.path.expanduser("~/Desktop/SilenceTracker/TrimmedAudioFiles/Participant"+str(num)+"Trim.wav")
-        sampFreq, theSoundFile = wavfile.read(filePathwayRead)
-        print("Opening participant " + str(num) + " file. . . " + filePathwayRead)
-    for index, amp in enumerate(theSoundFile):
-        if index % 10 == 0: 
-            if abs(amp) > threshold:
-                theSecond = round(index/100000, 0)
-                if counter == 0: #starting counter
-                    previousSecond = theSecond
-                    counter += 1
-                elif theSecond == 0:
-                    if num == 15:
-                        continue
-                    threshold+=100
-                elif theSecond == previousSecond: #continuing counter if the current time second is equal to the previous
-                    counter += 1
-                else:
-                    if counter > 50: #threshold being that if there are 50 .00001 seconds with sound
-                        soundTime.append(previousSecond) #appending a list of the times (s) a participant spoke
-                    counter = 0
-    if previousSecond > theSecond:
-        participantSoundTime[str(num)-1].append(previousSecond)
-    participantSoundTime[str(num)] = soundTime #adding completed list of times to a dictionary where participant # is key
+def getParRanges():
     soundTime = []
-    threshold = 1000 
+    participantSoundTime = {}
 
-print(participantSoundTime)
+    counter = 0
+    maxCount = 50
+    threshold = 1000
 
+    for num in range(1, 33):
+        if num == 11 or num == 13 or num == 14 or num == 16 or num == 21 or num == 22 or num == 26 or num == 27:
+            continue
+        else:
+            #tori path:
+            filePathwayRead = os.path.expanduser("~/Desktop/SilenceTracker/TrimmedAudioFiles/Participant"+str(num)+"Trim.wav")
+            #Jake path, just comment this out for the code to work on your end.  
+            filePathwayRead = os.path.expanduser("C:/Users/Jake From State Farm\Desktop/TrimmedAudioFiles/Participant"+str(num)+"Trim.wav")
+            sampFreq, theSoundFile = wavfile.read(filePathwayRead)
+            print("Opening participant " + str(num) + " file. . . " + filePathwayRead)
+            
+        for index, amp in enumerate(theSoundFile):
+            #checking every 10th ms
+            if index % 10 == 0:
+                
+                if abs(amp) > threshold:
+                    theSecond = round(index/100000, 0)
+                    
+                    #checking if our second is already in the list.  Should speed things up considerably
+                    if theSecond in soundTime:
+                        continue
+                    
+                    else:
+                        #checking if the signal is held for a short time
+                        continuousSignal = True
+                        #list comprehension from current position to 50 places out
+                        for amp in theSoundFile[index: index + maxCount]:
+                            if amp < threshold:
+                                    #if our amplitude drops below threshold, our signal is not continuous
+                                    continuousSignal = False
+                                    break
+                        #only append to list if this is true!!!!
+                        if continuousSignal == True:
+                            soundTime.append(theSecond)
+                            
+        #Changing the list format
+        soundTime = list(find_ranges(soundTime))
+        #adding completed list of times to a dictionary where participant # is key      
+        participantSoundTime[str(num)] = soundTime
+        #resetting everything
+        soundTime = []
+        threshold = 1000
+        
+    return participantSoundTime
+
+
+print(getParRanges())
 #if you want to see graphs of soundwave
 
 #timeArray = (arange(0, theSoundFile.shape[0], 1)/sampFreq)
