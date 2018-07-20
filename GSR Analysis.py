db4baaa219098357ddcd7300838ec85672e2db33
Jake Maranzatto <maranzatto1@yahoo.com>
2018-07-20 15:10:04 -0400

    updates
    smol

--- GSR Analysis.py
+++ GSR Analysis.py
@@ -11,7 +11,16 @@
 #3- transplant this analysis into the excel sheet
 
 #building blocks for first goal
+
+#make box and whisker plot
+#get gsr baseline
+
+
+
+# get first and second half gsr values across groups
 import openpyxl
+import pandas as pd
+from openpyxl import load_workbook
 import os
 import numpy as np
 from matplotlib.ticker import MaxNLocator
@@ -23,6 +32,7 @@ wb = openpyxl.load_workbook('C:\\Users\\Jake From State Farm\\Desktop\\Peaks pr 
 data_folder = 'C:/Users/Jake From State Farm/AppData/Local/Programs/Python/Python36-32/Sensor Data'
 sheet = wb['Peaks pr respondent']
 
+####################################DEPRECATED########################################
 ###generic function that can work for times, amplitudes, etc.
 ##def getSinglePeak(val_list, participant):
 ##    value = 0
@@ -91,6 +101,61 @@ sheet = wb['Peaks pr respondent']
 ##    return peak_dict
 
 ###########################################################################
+#################GETTING HEARTRATE DERIVATIVE#############################
+###########################################################################
+
+#yvals is a list or tuple of 2 values
+def getSlope(yvals, xdist):
+    return (yvals[1] - yvals[0]) / xdist
+
+#many_yvals is a list of arbitrary size
+def getDerivative(many_yvals, xdist):
+    #returns a list, may make it just write to file to save memory
+    deriv = []
+    previous = 0
+    for v in many_yvals:
+        if v != None and v > 0:
+            deriv.append(getSlope((previous, v), xdist))
+            previous = v
+        else:
+            deriv.append(0)
+            previous = 0
+
+    deriv_dict = {}
+    for i, d in enumerate(deriv):
+        if d != 0:
+            #making dictionary of structure {time(ms) : derivitive}
+            #this shows us when the derivitive changes and by how much
+            deriv_dict[i] = d
+    return deriv_dict
+
+def getSingleDer(file, xdist):
+        data = []
+        path = data_folder + '/' +  file
+        wrkbk = load_workbook(filename=path)
+        sheet = wrkbk.sheetnames[0]
+        sheet = wrkbk[sheet]
+
+        prev = 0
+        for cell in sheet['FJ']:
+            try:
+                cell.value += 1
+            except TypeError:
+                continue
+            data.append(cell.value)
+        print(data[0])
+        return getDerivative(data, xdist)
+
+def getAllDer(xdist):
+        #this may complain about double \\ in data_folder dir name
+        for file in os.listdir(data_folder):
+                getSingleDer(file, 1)
+                
+if 0:
+    min_to_ms = 1.66 * (10**-5)
+    print(getSingleDer('001_Participant 1.xlsx', 1))
+
+###########################################################################
 ########################FINDING GSR PEAKS##################################
 ###########################################################################
 
@@ -147,9 +212,9 @@ def histogramData(condition, activation_threshold, n_bins, single_or_total):
     return data
 
 
-if 1:
+if 0:
     ##PARAMETERS##
-    single_or_total = 1
+    single_or_total = 0
     timestep = 10
     threshold = 0.25
     ################
@@ -193,50 +258,30 @@ if 1:
 ##    print('monitor condition with threshold of ' + str(thresholds[i]) + ' ' + str(histogramData(0, thresholds[i], bins)))
 ##    print('VR condition with threshold of ' + str(thresholds[i]) +  ' ' + str(histogramData(1, thresholds[i], bins)))
 
-
-###########################################################################
-#################GETTING HEARTRATE DERIVATIVE#############################
-###########################################################################
-
-###yvals is a list or tuple of 2 values
-##def getSlope(yvals, xdist):
-##    return (yvals[1] - yvals[0]) / xdist
-##
-###many_yvals is a list of arbitrary size
-##def getDerivative(many_yvals, xdist):
-##    #returns a list, may make it just write to file to save memory
-##    deriv = []
-##    previous = 0
-##    for v in many_yvals:
-##        if v != None:
-##            deriv.append(getSlope((previous, v), xdist))
-##            previous = v
-##        else:
-##            deriv.append(0)
-##            previous = v
-##
-##    return deriv
-##
-##def getSingleDer(file, xdist):
-##        path = data_folder + '/' +  file
-##        wrkbk = openpyxl.load_workbook(path)
-##        sheet = wrkbk[0]
-##        print('a')
-##
-##def getAllDer(xdist):
-##        #this may complain about double \\ in data_folder dir name
-##        for file in os.listdir(data_folder):
-##                getSingleDer(file, 1)
-##                
-##
-##getAllDer(1)
-        
+###########################GSR MEAN SCORES###########################################
+##using pandas library here, trying out different libraries to see which ones i prefer
 
 
+def getSingleGSRAvg(file, startTIme, endTime):
+        path = data_folder + '/' +  file
+        df = pd.read_excel(path, header = None, usecols = [167])
+        vals = df.iloc[startTIme:endTime].mean()
+        try:
+            print(vals[0])
+            return vals[0]
+        except:
+            print(-1)
+            return -1
 
-
-
-
+def getAllAvg(startTIme, endTime, condition):
+        #this may complain about double \\ in data_folder dir name
+        arr = []
+        for i, file in enumerate(os.listdir(data_folder)):
+                if getCondition(i) == condition:
+                    arr.append(getSingleGSRAvg(file, startTIme, endTime))
+        return arr
+    
+print(getAllAvg(5, 10000, 0))
 
 
 
